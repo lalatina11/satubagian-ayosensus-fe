0d62f2e4ce580ff95814b569432ee8ee5feb7be0
lalatina11 <rahmadancandra@gmail.com>
2025-10-05 20:42:05 +0700

    feat: dashboard page  regency admin

---
+++ src/app/kabupaten/admin/page.tsx
@@ -0,0 +1,21 @@
+import LogoutForm from "@/components/kabupaten/forms/LogoutForm";
+import { getAdminKabupatenAuthInfo } from "@/lib/actions";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Admin Kab | Dashboard",
+};
+
+const Page = async () => {
+  const session = await getAdminKabupatenAuthInfo();
+  console.log(session);
+
+  return (
+    <div className="flex flex-col">
+      <h1>Welcome {session.name} in Admin Kab Dashboard</h1>
+      <LogoutForm />
+    </div>
+  );
+};
+
+export default Page;
