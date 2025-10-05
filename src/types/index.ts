f9d7df7ebf081e3fce30dc998abeefdf7066d18f
lalatina11 <rahmadancandra@gmail.com>
2025-10-05 21:22:10 +0700

    feat: AdminRegencySession explicit types

---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+
+export interface AdminRegencySession {
+    id: number;
+    name: string;
+    nik: string | null;
+    otp: string | null;
+    phone: string | null;
+    created_at: string;
+    updated_at: string;
+}
