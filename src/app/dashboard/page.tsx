4db469b674786fa3e8003138c18fa2d888eed6f1
lalatina11 <rahmadancandra@gmail.com>
2025-10-06 22:40:40 +0700

    feat: index dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,21 +1,14 @@
-import LogoutForm from "@/components/forms/LogoutForm";
 import { getAdminRegencyAuthInfo } from "@/lib/actions";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Admin Kab | Dashboard",
+  title: "Admin | Dashboard",
 };
 
 const Page = async () => {
-  const session = await getAdminRegencyAuthInfo();
-  console.log(session);
+  const userSession = await getAdminRegencyAuthInfo();
 
-  return (
-    <div className="flex flex-col">
-      <h1>Welcome {session?.name} in Admin Kab Dashboard</h1>
-      <LogoutForm />
-    </div>
-  );
+  return <div>Welcome {userSession?.name}</div>;
 };
 
 export default Page;
