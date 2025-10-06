f6e3defb691e8cfe820e2624be2066bd6f001703
lalatina11 <rahmadancandra@gmail.com>
2025-10-06 22:37:36 +0700

    feat: dashboard container

---
+++ src/components/containers/DashboardContainer.tsx
@@ -0,0 +1,31 @@
+import { UserRole } from "@/types";
+import { ReactNode } from "react";
+import { SidebarProvider, SidebarTrigger } from "../ui/sidebar";
+import { AppSidebar } from "../sidebar/AppSidebar";
+
+interface Props {
+  role: UserRole;
+  children: ReactNode;
+}
+const DashboardContainer = ({ children, role }: Props) => {
+  return (
+    <SidebarProvider>
+      <AppSidebar role={role} />
+      <main className="p-2 min-h-screen bg-background">
+        <div className="flex gap-2 items-center">
+          <SidebarTrigger />
+          <span className="font-semibold">
+            {role === "superadmin"
+              ? "Super Admin Dashboard"
+              : role === "admin"
+              ? "Admin Dashboard"
+              : "User Dashboard"}
+          </span>
+        </div>
+        <div className="p-1">{children}</div>
+      </main>
+    </SidebarProvider>
+  );
+};
+
+export default DashboardContainer;
