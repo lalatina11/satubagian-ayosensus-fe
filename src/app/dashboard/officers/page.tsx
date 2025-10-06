01896dd80c4b67b2c0b1cdb7b71f2c806aa3047e
lalatina11 <rahmadancandra@gmail.com>
2025-10-07 01:35:38 +0700

    feat: apply officers table and add officers form

--- src/app/dashboard/officers/page.tsx
+++ src/app/dashboard/officers/page.tsx
@@ -1,19 +1,33 @@
-import { getAdminRegencyAuthInfo } from "@/lib/actions";
+import OfficersTable from "@/components/OfficersTable";
+import { columns } from "@/components/OfficersTable/column";
+import { getAdminRegencyAuthInfo, getOfficersData } from "@/lib/actions";
+import { Officer } from "@/types";
 import { Metadata } from "next";
+import { AddOfficersForm } from "@/components/forms/AddOfficersForm";
 
 export const metadata: Metadata = {
-  title: "Admin Officers | Dashboard",
+    title: "Admin Officers | Dashboard",
 };
 
 const Page = async () => {
-  const authSession = await getAdminRegencyAuthInfo();
+    const authSession = await getAdminRegencyAuthInfo();
+    const officersRes = await getOfficersData();
+    const officers = officersRes.data as Array<Officer>;
 
-  return (
-    <div>
-      <h1>Welcome {authSession?.session.name}</h1>
-      <h1>Officers List</h1>
-    </div>
-  );
+    return (
+        <main className="flex flex-col gap-3 w-full">
+            <h1>Welcome {authSession?.session.name}</h1>
+            <section className="flex flex-col gap-3 p-5">
+                <AddOfficersForm/>
+            </section>
+            <section className="flex flex-col gap-3 p-5 rounded-md bg-card">
+                <h1 className="text-2xl font-semibold">Daftar Petugas</h1>
+                <div className="">
+                    <OfficersTable columns={columns} data={officers}/>
+                </div>
+            </section>
+        </main>
+    );
 };
 
 export default Page;
