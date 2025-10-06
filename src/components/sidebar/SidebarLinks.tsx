0e5f37bc4e7c05a2b0ebf6d0a335bf5e4d726cf7
lalatina11 <rahmadancandra@gmail.com>
2025-10-06 22:36:26 +0700

    feat: sidebar links rendering

---
+++ src/components/sidebar/SidebarLinks.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import Link from "next/link";
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "../ui/sidebar";
+import { UserRole } from "@/types";
+import { LayoutDashboard, Users } from "lucide-react";
+import { usePathname } from "next/navigation";
+
+interface Props {
+  role: UserRole;
+}
+
+const SidebarLinks = ({}: Props) => {
+  const items = [
+    {
+      title: "Dashboard",
+      url: "/dashboard",
+      icon: LayoutDashboard,
+    },
+    {
+      title: "Petugas",
+      url: "/dashboard/officers",
+      icon: Users,
+    },
+  ];
+
+  const pathname = usePathname();
+
+  return (
+    <SidebarGroup>
+      <SidebarGroupLabel>Links</SidebarGroupLabel>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {items.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton
+                variant={item.url === pathname ? "outline" : "default"}
+                asChild
+              >
+                <Link href={item.url}>
+                  <item.icon />
+                  <span>{item.title}</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  );
+};
+
+export default SidebarLinks;
