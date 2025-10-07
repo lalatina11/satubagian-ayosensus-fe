061738124566927338f5cd25f06ba1d8f1a9ae13
lalatina11 <rahmadancandra@gmail.com>
2025-10-07 08:50:57 +0700

    refactor: revalidation
    accept json only, and handling the errors

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,86 +1,105 @@
-"use server"
+"use server";
 
-import z from "zod"
-import { ENV } from "./env"
-import { loginAdminRegencySchema } from "./schemas"
-import { cookies } from "next/headers"
-import { UserSession } from "@/types"
-import { redirect } from "next/navigation"
+import z from "zod";
+import { ENV } from "./env";
+import { loginAdminRegencySchema } from "./schemas";
+import { cookies } from "next/headers";
+import { UserSession } from "@/types";
+import { redirect } from "next/navigation";
 import { OfficersFormValues } from "@/components/forms/AddOfficersForm";
 import { revalidatePath } from "next/cache";
 
 export const getAccessToken = async () => {
-    const cookie = await cookies()
-    return cookie.get("access_token")?.value
-}
+  const cookie = await cookies();
+  return cookie.get("access_token")?.value;
+};
 
 export const getCurrentSession = async (token: string) => {
-    return await fetch(`${ENV.NEXT_PUBLIC_BACKEND_API_BASE_URL}/me`, { headers: { Authorization: `Bearer ${token}` } })
-}
+  return await fetch(`${ENV.NEXT_PUBLIC_BACKEND_API_BASE_URL}/me`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+};
 
-export const handleLoginAdminRegency = async (values: z.infer<typeof loginAdminRegencySchema>) => {
-    const cookie = await cookies()
-    const res = await fetch(`${ENV.NEXT_PUBLIC_BACKEND_API_BASE_URL}/login`, {
-        method: "POST",
-        body: JSON.stringify(values),
-        headers: { "Content-Type": "application/json" }
-    })
-    const result = await res.json()
-    if (result.error) {
-        throw new Error(result.error || "Something went wrong!")
-    }
-    cookie.set("access_token", result.access_token, {
-        path: "/",
-        maxAge: 60 * 60 * 24 * 2,
-        httpOnly: true,
-        sameSite: "lax",
-        secure: ENV.NODE_ENV === "production"
-    })
-}
+export const handleLoginAdminRegency = async (
+  values: z.infer<typeof loginAdminRegencySchema>,
+) => {
+  const cookie = await cookies();
+  const res = await fetch(`${ENV.NEXT_PUBLIC_BACKEND_API_BASE_URL}/login`, {
+    method: "POST",
+    body: JSON.stringify(values),
+    headers: { "Content-Type": "application/json" },
+  });
+  const result = await res.json();
+  if (result.error) {
+    throw new Error(result.error || "Something went wrong!");
+  }
+  cookie.set("access_token", result.access_token, {
+    path: "/",
+    maxAge: 60 * 60 * 24 * 2,
+    httpOnly: true,
+    sameSite: "lax",
+    secure: ENV.NODE_ENV === "production",
+  });
+};
 
 export const getAdminRegencyAuthInfo = async () => {
-    const token = await getAccessToken()
-    const res = await getCurrentSession(token as string)
-    if (!res.ok) {
-        return redirect("/login")
-    }
-    return { session: await res.json() as UserSession, token }
-
-}
+  const token = await getAccessToken();
+  const res = await getCurrentSession(token as string);
+  if (!res.ok) {
+    return redirect("/login");
+  }
+  return { session: (await res.json()) as UserSession, token };
+};
 export const handleLogOutAdminRegency = async () => {
-    const cookie = await cookies()
-    const token = await getAccessToken()
-    const res = await fetch(`${ENV.NEXT_PUBLIC_BACKEND_API_BASE_URL}/logout`, {
-        method: "POST",
-        headers: { Authorization: `Bearer ${token}` }
-    })
-    const result = await res.json()
-    if (result.error) {
-        throw new Error(result.error || "Something went wrong!")
-    }
-    cookie.delete("access_token")
-}
+  const cookie = await cookies();
+  const token = await getAccessToken();
+  const res = await fetch(`${ENV.NEXT_PUBLIC_BACKEND_API_BASE_URL}/logout`, {
+    method: "POST",
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  const result = await res.json();
+  if (result.error) {
+    throw new Error(result.error || "Something went wrong!");
+  }
+  cookie.delete("access_token");
+};
 
 export const getOfficersData = async () => {
-    const token = await getAccessToken()
+  const token = await getAccessToken();
 
-    const res = await fetch(`${ENV.NEXT_PUBLIC_BACKEND_API_BASE_URL}/officer`, {
-        headers: { Authorization: `Bearer ${token}` },
-        next: { tags: [ "officers" ] }
-    })
-    console.log(res);
+  const res = await fetch(`${ENV.NEXT_PUBLIC_BACKEND_API_BASE_URL}/officer`, {
+    headers: { Authorization: `Bearer ${token}` },
+    next: { tags: ["officers"] },
+  });
+  console.log(res);
 
-    return await res.json()
-}
+  return await res.json();
+};
 
 export const handleAddOfficers = async (values: OfficersFormValues) => {
-    const token = await getAccessToken()
-    const res = await fetch(`${ENV.NEXT_PUBLIC_BACKEND_API_BASE_URL}/officer`, {
-        method: "POST",
-        headers: { Authorization: `Bearer ${token}`, "Content-Type": "application/json" },
-        body: JSON.stringify({ data: values.officers }),
-    })
-    console.log(res)
-    if (!res.ok) throw new Error(`Error: ${res.status}`)
-    revalidatePath("officers")
-}
+  const token = await getAccessToken();
+  const res = await fetch(`${ENV.NEXT_PUBLIC_BACKEND_API_BASE_URL}/officer`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({ data: values.officers }),
+  });
+
+  if (!res.ok) {
+    throw new Error(
+      "Terjadi Kesalahan, cobalah untuk mengganti username petugas atau kode desa!",
+    );
+  }
+
+  const result = await res.json();
+  if (result.errors) {
+    throw new Error(
+      "Terjadi Kesalahan, cobalah untuk mengganti username petugas atau kode desa!",
+    );
+  }
+  revalidatePath("officers");
+};
+
