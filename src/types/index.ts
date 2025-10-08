aa3762a51c4b65ce0822696b17a59b5eff6fcd5a
lalatina11 <rahmadancandra@gmail.com>
2025-10-08 12:45:37 +0700

    feat: Village types explicit

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,23 +1,30 @@
-export type UserRole = "superadmin" | "admin" | "user"
+export type UserRole = "superadmin" | "admin" | "user";
 
 export interface UserSession {
-    id: string,
-    name: string,
-    role: Array<UserRole>,
-    first_name: null,
-    middle_name: null,
-    last_name: null,
-    kode_desa: null,
-    nik: null,
-    phone: null
+  id: string;
+  name: string;
+  role: Array<UserRole>;
+  first_name: null;
+  middle_name: null;
+  last_name: null;
+  kode_desa: null;
+  nik: null;
+  phone: null;
 }
 
 export interface Officer {
-    first_name: string | null
-    id: number
-    is_active: number
-    kode_desa: string | null;
-    last_name: string | null;
-    middle_name: string | null;
-    name: string
+  first_name: string | null;
+  id: number;
+  is_active: number;
+  kode_desa: string | null;
+  last_name: string | null;
+  middle_name: string | null;
+  name: string;
 }
+
+export type Village = {
+  kode: number;
+  name: string;
+  subdistrict: string;
+  subdistrict_kode: number;
+};
