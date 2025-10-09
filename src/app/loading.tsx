99718c150d574d2b6f6c268e3836c3df2e5eec89
lalatina11 <rahmadancandra@gmail.com>
2025-10-09 22:43:00 +0700

    feat: apply custom loading spinner

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,5 +1,11 @@
+import SpinnerCustom from "@/components/Loading/Spinner";
+
 const Loading = () => {
-  return <div>Loading</div>;
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <SpinnerCustom className="size-40" />
+    </div>
+  );
 };
 
 export default Loading;
