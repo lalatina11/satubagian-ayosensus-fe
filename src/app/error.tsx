f5a591f2898b56a4d52a37432445de673bf88d36
lalatina11 <rahmadancandra@gmail.com>
2025-10-10 07:53:08 +0700

    chore: simple error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,6 +1,12 @@
 "use client";
+
 const Error = () => {
-  return <div>Error Page is here</div>;
+  return (
+    <div className="flex flex-col gap-4 text-3xl min-h-screen">
+      <span>Terjadi Kesalahan Server</span>
+      <span>Coba Lagi beberapa Saat</span>
+    </div>
+  );
 };
 
 export default Error;
